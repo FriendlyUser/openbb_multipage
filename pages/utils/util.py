6d5a2137b282bd20129d94fc4f8b8d6254521ba3
David Li <davidli012345@gmail.com>
2023-04-10 08:51:59 -0700

    fix: try again

--- pages/utils/util.py
+++ pages/utils/util.py
@@ -7,15 +7,6 @@ def remove_existing_file(func):
     def wrapper(*args, **kwargs):
         old_stdin = sys.stdin
         sys.stdin = StringIO("y")
-        stream = os.popen('cd ~ && pwd')
-        root_dir = stream.read()
-        # check for .openbb_terminal/env in root_dir
-        if os.path.exists(os.path.join(root_dir.strip(), ".openbb_terminal", ".env")) == False:
-            print("Did not find .openbb_terminal/.env")
-            # create it
-            os.mkdir(os.path.join(root_dir.strip(), ".openbb_terminal"))
-            # make .env file
-            env_file = open(os.path.join(root_dir.strip(), ".openbb_terminal", ".env"), "w")
             
         sample_dir = root_dir.strip()
         # remove /home/codespace/OpenBBUserData/exports/bbands.png already
