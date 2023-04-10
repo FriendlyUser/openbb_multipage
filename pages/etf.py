0ccb7790c2d5164da30da78eb9c274de33c7bc32
David Li <davidli012345@gmail.com>
2023-04-09 21:06:29 -0700

    fix: update symbols

--- pages/etf.py
+++ pages/etf.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
 from openbb_terminal.etf.stockanalysis_model import get_all_names_symbols
 
 etf_symbols = get_all_names_symbols()
-
-st.write(etf_symbols)
+etf_dict = {'Symbols': etf_symbols[0], 'Names': etf_symbols[1]}
+etf_df = pd.DataFrame(etf_dict)
+st.write(etf_df)
