c9d624f0606d27ff310981055454756759d9dc6b
David Li <davidli012345@gmail.com>
2023-04-09 20:50:24 -0700

    fix: adding news module and more

---
+++ pages/news.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from openbb_terminal import get_news
+
+def fetch_news(keyword):
+    # This is where you would call a news API or scrape a news website
+    # to get news articles related to the user's keyword
+    # For this example, we'll just return a string with the keyword
+    return get_news(keyword)
+
+# Create a text input and store the user's input in a variable
+user_input = st.text_input("Enter a keyword to search for news articles")
+
+# Call the get_news function with the user's input and display the results
+if user_input:
+    news = get_news(user_input)
+    st.write(news)
